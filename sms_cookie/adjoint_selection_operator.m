710de09629a76e8e0669d07f8854a76725917570
Omer Burak Demirel <50053603+obdemirel@users.noreply.github.com>
2023-03-25 10:31:09 -0500

    Delete sms_cookie directory

--- sms_cookie/adjoint_selection_operator.m
+++
@@ -1,7 +0,0 @@
-function y = adjoint_selection_operator(x, locations, m,n,no_c)
-
-space = zeros(m,n,no_c,'single');
-space(locations) = x;
-y = space(:);
-
-end
--- sms_cookie/selection_operator.m
+++
@@ -1,8 +0,0 @@
-function y = selection_operator(x, locations, m, n, no_c)
-
-x = reshape(x,m,n,no_c);
-
-y = x(locations);
-y = y(:);
-
-end
